d7b68ebe7e261c7fff68641afca0b7e96f409255
민성 김 <ms930524@naver.com>
2022-03-05 13:55:58 +0900

    test upload

---
+++ MINZZY_brunch_test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MINZZY_brunch_test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //test
+        }
+    }
+}
